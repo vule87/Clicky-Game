36846b6d0da4f61355a4c0870acbce18e3ee20b7
Vu Le <vuleuml2011@gmail.com>
2019-01-14 15:41:11 -0600

    Updates

---
+++ precache-manifest.b759700ddce9b7745f901243a89d7b39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "6acb6692e4ee25941da6",
+    "url": "/clicky-game/static/js/runtime~main.6acb6692.js"
+  },
+  {
+    "revision": "c4d5131eeab81777a601",
+    "url": "/clicky-game/static/js/main.c4d5131e.chunk.js"
+  },
+  {
+    "revision": "405fe43fb4b888bea6c1",
+    "url": "/clicky-game/static/js/1.405fe43f.chunk.js"
+  },
+  {
+    "revision": "c4d5131eeab81777a601",
+    "url": "/clicky-game/static/css/main.06a8fa71.chunk.css"
+  },
+  {
+    "revision": "76ff0d518323ee1c589d0700ec5d1be8",
+    "url": "/clicky-game/index.html"
+  }
+];
